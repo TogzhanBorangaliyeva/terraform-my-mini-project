c50f66393e7083a9548f20bb790a93d895d8b4ce
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-01-09 15:09:24 -0600

    added rds with secrets manager

--- root/main.tf
+++ root/main.tf
@@ -9,3 +9,22 @@ module "vpc" {
     cidr_route_table_private = var.root_cidr_route_table_private
     cidr_everywhere = var.root_cidr_everywhere
 }
+
+module "rds" {
+    source = "../rds" 
+    db_instance_name = var.root_db_instance_name
+    db_allocated_storage = var.root_db_allocated_storage
+    db_engine = var.root_db_engine
+    db_engine_version = var.root_db_engine_version
+    db_instance_class = var.root_db_instance_class
+    secret_name = var.root_secret_name
+    secret_data = jsonencode({
+        DB_USERNAME = var.db_username
+        DB_PASSWORD = var.db_password
+        DB_NAME     = var.db_name
+    })
+
+    # Pass the private subnets output from the VPC module to the RDS module
+    private_subnet_ids = module.vpc.private_subnet_ids
+    vpc_id = module.vpc.vpc_id
+}
